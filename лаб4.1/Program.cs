8f4a258e20fbaea92a6b712cdca2ba8f6ecb7657
RobotComp.ru <RobotComp.ru@DESKTOP-94TEC7C>
2024-11-07 22:44:53 +0300

    Добавьте файлы проекта.

---
+++ лаб4.1/Program.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.Write("Введите размер массива: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        
+        int[] arr = new int[n];
+
+        Console.WriteLine("Введите элементы массива:");
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        
+        double sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += arr[i];
+        }
+        double average = sum / n;
+
+       
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] > average)
+            {
+                count++;
+            }
+        }
+
+       
+        double percentage = ((double)count / n) * 100;
+
+     
+        Console.WriteLine($"Процент элементов, превышающих среднеарифметическое: {percentage:F2}%");
+    }
+}
